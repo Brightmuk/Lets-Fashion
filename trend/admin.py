3980016610cd3d0c0d793f1786d830ad1d5992aa
Brightmuk <be.rightmuk@gmail.com>
2019-07-10 09:10:25 +0300

    Registering my models to admin

--- trend/admin.py
+++ trend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product,Profile,Comment
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Product)
+admin.site.register(Comment)
--- trend/views.py
+++ trend/views.py
@@ -1,3 +1,10 @@
+from django.shortcuts import render,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from django.shortcuts import render,redirect
+from .models import Product,Profile,Comment
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 
-# Create your views here.
